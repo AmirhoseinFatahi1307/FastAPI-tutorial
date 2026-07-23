7e12aef4d8ba8d8ebf15c4d6a6559954e4000666
Amir <fatahiamirhosein8@gmail.com>
2026-07-23 20:41:51 +0330

    Form : estefade dar jahayi ke nabayad dar url namayesh dade shavad baraye mesal dar username, password, update name etc

--- core/main.py
+++ core/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, status, HTTPException, Path
+from fastapi import FastAPI, Query, status, HTTPException, Path, Form
 from fastapi.responses import JSONResponse
 import random
 from typing import Annotated
@@ -30,7 +30,7 @@ def retireve_names_list(
 
 # /names (GET(RETRIEVE), POST(CREATE))
 @app.post("/names", status_code=status.HTTP_201_CREATED)
-def create_name(name: str):
+def create_name(name: str = Form()):
     name_obj = {"id": random.randint(6, 100), "name": name}
     names_list.append(name_obj)
     return f"id : {name_obj["id"]} and name : {name_obj["name"]}"
@@ -55,7 +55,7 @@ def retrieving_name_detail(
 
 # /names/:id (GET(RETRIEVE), PUT/PATCH(UPDATE), DELETE(DELETE))
 @app.put("/names/{name_id}", status_code=status.HTTP_200_OK)
-def Update_name_detail(name_id: int, name: str):
+def Update_name_detail(name_id: int = Path(), name: str = Form()):
     for item in names_list:
         if item["id"] == name_id:
             item["name"] = name
