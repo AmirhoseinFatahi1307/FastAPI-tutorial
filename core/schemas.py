4f47f0e311d304235f95e0c04800cb3e3a86fd82
Amir <fatahiamirhosein8@gmail.com>
2026-08-03 19:20:17 +0330

    Pydantic Validators: validators hy bareye vorodi ha ke az ghaveed khasi peyravi konan

--- core/schemas.py
+++ core/schemas.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class BasePersonSchema(BaseModel):
     name: str
+
+    @field_validator("name")
+    def validate_name(cls, value):
+        if len(value) > 32:
+            raise ValueError("Name must not exceed 32 characters ")
+        if not value.isalpha():
+            raise ValueError("Name must contain only alphabetic characters")
+        return value
 
 
 class PersonCreateSchema(BasePersonSchema):
