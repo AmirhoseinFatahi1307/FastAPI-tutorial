3a3e54d1753d1e4aa2f7f3be8dff81bda2d3853e
Amir <fatahiamirhosein8@gmail.com>
2026-08-03 19:43:15 +0330

    Field , field_serializer: hamanand validator baresi shart hayi mikaonad va taghirati dar vorodi ha midahad mesal dar field : mitanim default value set konim allias va.... dar field_serializer : mitavan serialize kard vorodi ro

--- core/schemas.py
+++ core/schemas.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, field_validator, Field, field_serializer
 
 
 class BasePersonSchema(BaseModel):
-    name: str
+    name: str = Field(..., description="Enter Persons name")
 
     @field_validator("name")
     def validate_name(cls, value):
@@ -12,13 +12,17 @@ class BasePersonSchema(BaseModel):
             raise ValueError("Name must contain only alphabetic characters")
         return value
 
+    @field_serializer("name")
+    def serialize_name(value):
+        return value.title()
+
 
 class PersonCreateSchema(BasePersonSchema):
     pass
 
 
 class PersonResponseSchema(BasePersonSchema):
-    id: int
+    id: int = Field(..., description="Unique user identifier")
 
 
 class PersonUpdateSchema(BasePersonSchema):
