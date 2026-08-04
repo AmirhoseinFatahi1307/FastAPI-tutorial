fc199779a3403971f39df0262dcc9b92526e379b
Amir <fatahiamirhosein8@gmail.com>
2026-08-04 21:02:07 +0330

    sql alchemy fields

--- core/database_test.py
+++ core/database_test.py
@@ -1,4 +1,25 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    Float,
+    Numeric,
+    Date,
+    DateTime,
+    Time,
+    Text,
+    Interval,
+    Enum,
+    ARRAY,
+    JSON,
+    ForeignKey,
+    LargeBinary,
+    UUID,
+    create_engine,
+)
+from enum import Enum as PythonEnum
+from sqlalchemy.orm import relationship
 
 # from sqlalchemy ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
@@ -32,6 +53,34 @@ class User(Base):
 
     def __repr__(self):
         return f"User (id = {self.id}, firstname = {self.firstname}, lastname = {self.lastname})"
+
+
+class UserType(PythonEnum):
+    ADMIN = "admin"
+    USER = "user"
+    GUEST = "guest"
+
+
+class SampleModel(Base):
+    __tablename__ = "sample_model"
+
+    id = Column(Integer, primary_key=True)
+    string_field = Column(String(100))
+    text_field = Column(Text)
+    boolean_field = Column(Boolean)
+    integer_field = Column(Integer)
+    float_field = Column(Float)
+    numeric_field = Column(Numeric(10, 2))
+    date_field = Column(Date)
+    datetime_field = Column(DateTime)
+    time_field = Column(Time)
+    interval_field = Column(Interval)
+    enum_field = Column(Enum(UserType))
+    array_field = Column(ARRAY(Integer))
+    json_field = Column(JSON)
+    uuid_field = Column(UUID)
+    foreign_key_field = Column(Integer, ForeignKey("related_table.id"))
+    binary_field = Column(LargeBinary)
 
 
 # to create tables and databases
