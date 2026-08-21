fb33e81e19837108b21ea45a1b12d443a088a5ac
Amir <fatahiamirhosein8@gmail.com>
2026-08-21 16:26:13 +0330

    modifing the databse with the url inside of the config.py

--- core/database.py
+++ core/database.py
@@ -23,16 +23,17 @@ from sqlalchemy import (
 )
 from enum import Enum as PythonEnum
 from sqlalchemy.orm import relationship
+from config import settings
 
 # from sqlalchemy ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitemain.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlitemain.db"
 # SQLALCHEMY_DATABASE_URL = "sqllite:///:memory:"
 
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
+    settings.SQLALCHEMY_DATABASE_URL,
     connect_args={"check_same_thread": False},  # only for sqlite
 )
 Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
